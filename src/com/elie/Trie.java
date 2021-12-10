7c2e74f03faa3ca35fd1941e1268a3fed0ca0330
wxc5402@gmail.com <wxc5402@gmail.com>
2021-12-10 14:31:36 -0500

    first commit

---
+++ src/com/elie/Trie.java
@@ -0,0 +1,59 @@
+package com.elie;
+
+class Trie {
+
+    TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                node.children[ch - 'a'] = new TrieNode();
+            }
+            node = node.children[ch - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+
+        return node.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return false;
+            }
+            node = node.children[ch - 'a'];
+        }
+        return true;
+    }
+}
+
+class TrieNode {
+    TrieNode[] children;
+    boolean isWord;
+    public TrieNode() {
+        children = new TrieNode[26];
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
