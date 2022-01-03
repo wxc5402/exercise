3e04865147804308cc66493bb554bda4ca6aec8f
wxc5402@gmail.com <wxc5402@gmail.com>
2022-01-03 17:18:27 -0500

    meeting room

--- src/com/elie/Main.java
+++ src/com/elie/Main.java
@@ -17,6 +17,55 @@ public class Main {
 
        Integer[] root = {1,2,3,4,5,6,7,8,9};
        TreeNode rootHead = TreeNodeUtil.createTreeNode(root);
-       TreeNodeUtil.prettyPrintTree(rootHead);
+       //TreeNodeUtil.prettyPrintTree(rootHead);
+        String a = "ddd";
+        String[] dd = a.split(" ");
+        System.out.println(lengthOfLastWord("HELLO WORLD       "));
+    }
+
+
+    public String reverseWords(String s) {
+        String[] ret = s.split(" ");
+        if (ret == null || ret.length == 0) {
+            return "";
+        }
+
+        StringBuilder retStr = new StringBuilder();
+        for (String str : ret) {
+            retStr.append(reverseAWords(str));
+        }
+
+        return retStr.toString();
+    }
+
+    private String reverseAWords(String str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+        int[] ret = new int[str.length()];
+        for (int index = 0; index < str.length(); index++) {
+            ret[str.length() - index - 1] = str.charAt(index);
+        }
+
+        return new String(ret, 0, str.length());
+    }
+
+
+    public static int lengthOfLastWord(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        for(int i = s.length()-1; i >= 0; i--){
+            if(s.charAt(i) == ' '){
+                if(count == 0) {
+                    continue;
+                }
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
