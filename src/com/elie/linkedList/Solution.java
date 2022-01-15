7f0833fb11a9fd1fb4fc7dbd6e0a832a5f318fef
wxc5402@gmail.com <wxc5402@gmail.com>
2022-01-14 22:35:00 -0500

    meeting room

--- src/com/elie/linkedList/Solution.java
+++ src/com/elie/linkedList/Solution.java
@@ -66,11 +66,3 @@ public class Solution {
     }
 }
 
-
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
